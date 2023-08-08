f469ffb8bd7218e7e2c85af0135609a6a24fe28c
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2023-08-08 14:00:01 +0200

    Refactor ERC-721 transfer logic

--- gas_optimizer.py
+++ gas_optimizer.py
@@ -2,3 +2,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Add support for multiple consensus mechanisms
 // Add support for zk-SNARKs in smart contracts
+// Optimize contract storage for gas reduction
