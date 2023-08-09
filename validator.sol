73dfd6ec4f908a36d62da471fd592c87545370d8
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2023-08-09 14:00:02 +0200

    Improve token burning mechanism

--- validator.sol
+++ validator.sol
@@ -1,3 +1,4 @@
 // Refactor decentralized governance structure
 // Add support for delegation in governance
 // Implement zero-knowledge proof (ZKP) support
+// Implement automatic transaction retries
