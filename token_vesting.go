83f015bf54b59251ce795b6558e44d1cd348fb54
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2023-08-13 14:00:02 +0200

    Optimize storage for off-chain data

--- token_vesting.go
+++ token_vesting.go
@@ -1,3 +1,4 @@
 // Improve gas fee estimation accuracy
 // Improve decentralized lending contract flexibility
 // Refactor incentive structure in DeFi protocol
+// Optimize state channels for faster transactions
