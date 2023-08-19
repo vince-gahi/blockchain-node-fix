5d5ce3343ba54798794454e52cb7d92e572a0280
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2023-08-19 14:00:01 +0200

    Refactor contract to use EIP-712 signatures

--- proposal_handler.go
+++ proposal_handler.go
@@ -1,3 +1,4 @@
 // Optimize contract storage for gas reduction
 // Improve decentralized lending contract flexibility
 // Fix memory leak in Rust smart contract
+// Refactor governance proposal logic
