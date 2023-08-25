16a888252f9229c736ea4cbfdb6ee6c1eaa1c979
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2023-08-25 14:00:00 +0200

    Fix bugs in NFT metadata handling

--- blockchain_developer_documentation.rs
+++ blockchain_developer_documentation.rs
@@ -2,3 +2,4 @@
 // Add slashing mechanism for malicious actors
 // Fix issues with contract finalization
 // Implement cross-platform contract compatibility
+// Refactor storage layout for better efficiency
