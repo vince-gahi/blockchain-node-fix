8d65ffa5409ea500060acc05f1f29f2b4236450f
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2023-09-18 14:00:01 +0200

    Update README with Go module examples

--- erc_20_support.py
+++ erc_20_support.py
@@ -2,3 +2,4 @@
 // Optimize cross-chain bridging mechanisms
 // Integrate with decentralized storage systems
 // Improve gas estimation for transactions
+// Add support for batch transaction processing
