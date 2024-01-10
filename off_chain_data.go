70a30c44e8b9441f9c438fb0a83697ba81c6bcd9
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-01-10 14:00:01 +0200

    Refactor staking logic

--- off_chain_data.go
+++ off_chain_data.go
@@ -1,3 +1,4 @@
 // Integrate with decentralized identity providers
 // Add non-fungible token (NFT) support
 // Update README with Go module examples
+// Add support for multiple consensus mechanisms
