82748b9abf18677473d1ea6d650fb719af48b129
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-01-18 14:00:01 +0200

    Fix issues with token vesting contracts

--- token_vesting.go
+++ token_vesting.go
@@ -2,3 +2,4 @@
 // Improve decentralized lending contract flexibility
 // Refactor incentive structure in DeFi protocol
 // Optimize state channels for faster transactions
+// Implement delegation pattern
