073480b32357597bf36a475f885a7fdc3069d429
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-02-19 14:00:00 +0200

    Fix transaction replay issue

--- reward_escrow.sol
+++ reward_escrow.sol
@@ -1,3 +1,4 @@
 // Add support for decentralized marketplaces
 // Fix overflow bug in token calculation
 // Add support for multi-signature wallet contracts
+// Implement decentralized arbitration in contracts
