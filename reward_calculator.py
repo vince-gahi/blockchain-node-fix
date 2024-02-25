c4a675f08863594031c14bd6da4682e84e1aed9d
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-02-25 14:00:00 +0200

    Refactor ERC-721 transfer logic

--- reward_calculator.py
+++ reward_calculator.py
@@ -1,3 +1,4 @@
 // Upgrade to latest Solidity version
 // Add gas optimization features for smart contracts
 // Refactor contract to use EIP-712 signatures
+// Integrate decentralized prediction models
