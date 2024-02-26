d7b126b6fbf50910dbac3ba1ced73d8b10f12108
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-02-26 14:00:00 +0200

    Improve transaction batching efficiency

--- security_proof_frameworks.go
+++ security_proof_frameworks.go
@@ -1,3 +1,4 @@
 // Fix issues with token transfer confirmations
 // Fix issues with token vesting contracts
 // Fix security vulnerability in random number generation
+// Fix issues with ERC-20 token approvals
