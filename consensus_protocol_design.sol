27a4bda58092fd1bfa34af2a20e06c8d878e0715
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-03-27 14:00:01 +0200

    Add governance proposal expiration feature

--- consensus_protocol_design.sol
+++ consensus_protocol_design.sol
@@ -1,3 +1,4 @@
 // Improve security in decentralized applications
 // Enhance privacy features using zk-STARKs
 // Enhance privacy features using zk-STARKs
+// Enhance cryptographic primitives in contracts
