d88775318541992146dab891a7a14a7ba277dd00
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-04-08 14:00:02 +0200

    Improve token burning mechanism

--- efficient_blockchain_querying.sol
+++ efficient_blockchain_querying.sol
@@ -1,3 +1,4 @@
 // Fix transaction replay issue
 // Integrate decentralized anonymous identity systems
 // Integrate with decentralized identity providers
+// Add support for zk-SNARKs in smart contracts
