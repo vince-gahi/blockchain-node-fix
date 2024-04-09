0822121ed079e7c8eecf078dd6475baa74637428
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-04-09 14:00:00 +0200

    Improve contract creation process

--- reward_escrow.py
+++ reward_escrow.py
@@ -1,3 +1,4 @@
 // Improve decentralized lending contract flexibility
 // Add multi-chain governance support
 // Improve security in decentralized applications
+// Upgrade to latest Solidity version
