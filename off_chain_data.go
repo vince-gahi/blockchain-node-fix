53f1079e0631ad1ec49cb5e6a76377ee79086ac9
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-04-23 14:00:01 +0200

    Add support for Layer-2 solutions

--- off_chain_data.go
+++ off_chain_data.go
@@ -3,3 +3,4 @@
 // Update README with Go module examples
 // Add support for multiple consensus mechanisms
 // Optimize cross-chain bridging mechanisms
+// Improve error handling in transaction flow
