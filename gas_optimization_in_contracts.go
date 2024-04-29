20b800e22c1f39b622db3b1ca61ead8907f352db
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-04-29 14:00:00 +0200

    Refactor voting mechanism

--- gas_optimization_in_contracts.go
+++ gas_optimization_in_contracts.go
@@ -1,3 +1,4 @@
 // Refactor cross-chain communication layer
 // Integrate with decentralized identity providers
 // Fix interoperability issues with other blockchains
+// Optimize token minting flow
