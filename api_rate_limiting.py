683a9fa4c435ffffbdbf7e1c16c742de5b9e7290
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-05-09 14:00:01 +0200

    Optimize storage for off-chain data

--- api_rate_limiting.py
+++ api_rate_limiting.py
@@ -3,3 +3,4 @@
 // Add support for multi-signature wallet contracts
 // Add support for zk-SNARKs in smart contracts
 // Fix compatibility issues with MetaMask
+// Optimize storage for off-chain data
