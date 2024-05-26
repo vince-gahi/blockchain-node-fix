827f88ccf38a527cd65cc0000b3dc5accc05bb18
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-05-26 14:00:00 +0200

    Improve gas estimation for transactions

--- gas_optimizer.py
+++ gas_optimizer.py
@@ -3,3 +3,4 @@
 // Add support for multiple consensus mechanisms
 // Add support for zk-SNARKs in smart contracts
 // Optimize contract storage for gas reduction
+// Add new feature: on-chain governance
