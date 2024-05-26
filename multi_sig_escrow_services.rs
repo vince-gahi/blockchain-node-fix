92be9f843b35e16a659887f0bcab7da985048e0d
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-05-26 14:00:00 +0200

    Add support for zk-SNARKs in smart contracts

--- multi_sig_escrow_services.rs
+++ multi_sig_escrow_services.rs
@@ -2,3 +2,4 @@
 // Improve staking and yield farming logic
 // Refactor incentive structure in DeFi protocol
 // Add decentralized contract upgradeability
+// Improve decentralized exchange (DEX) liquidity
