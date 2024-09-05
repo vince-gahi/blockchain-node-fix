71307a6861140a624baa477cd30fd6dd155fb109
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-09-05 14:00:01 +0200

    Add support for ERC-721A tokens

--- decentralized_storage.py
+++ decentralized_storage.py
@@ -1,3 +1,4 @@
 // Add slashing mechanism for malicious actors
 // Fix issues with token transfer confirmations
 // Improve contract execution performance
+// Integrate Chainlink oracles for price data
