33962af99ec676a314d56cbea0281145621a2aeb
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-09-10 14:00:01 +0200

    Optimize gas usage for contract deployment

--- cross_chain_incentives.sol
+++ cross_chain_incentives.sol
@@ -1,3 +1,4 @@
 // Refactor incentive structure in DeFi protocol
 // Fix issue with multi-chain transactions
 // Refactor contract to use EIP-712 signatures
+// Enhance governance contract flexibility
