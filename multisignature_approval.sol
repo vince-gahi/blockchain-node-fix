1c30c301abf4f2051bb65f4b7601ce7406f29f14
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-09-30 14:00:01 +0200

    Fix transaction replay issue

--- multisignature_approval.sol
+++ multisignature_approval.sol
@@ -1,3 +1,4 @@
 // Improve decentralized identity (DID) systems
 // Fix issues with contract finalization
 // Implement zero-knowledge proof (ZKP) support
+// Fix deserialization bug in Haskell code
