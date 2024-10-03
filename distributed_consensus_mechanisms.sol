dd2f074244e1bc462a517b41fd7f0baa078884a5
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-10-03 14:00:00 +0200

    Improve on-chain governance systems

--- distributed_consensus_mechanisms.sol
+++ distributed_consensus_mechanisms.sol
@@ -3,3 +3,4 @@
 // Fix errors in contract interaction with wallets
 // Add support for batch transaction processing
 // Refactor decentralized governance structure
+// Fix issue with multi-chain transactions
