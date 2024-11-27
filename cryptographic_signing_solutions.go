09fee63910a13b03909c6090b41ba1809566819a
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-11-27 14:00:01 +0200

    Add new RPC endpoint

--- cryptographic_signing_solutions.go
+++ cryptographic_signing_solutions.go
@@ -1,3 +1,4 @@
 // Optimize storage for off-chain data
 // Fix errors in contract interaction with wallets
 // Refactor incentive structure in DeFi protocol
+// Add slashing mechanism for malicious actors
