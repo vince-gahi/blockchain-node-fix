2c4be6da94026870f50c928c675dc3d5e867394f
vince-gahi <157871773+vince-gahi@users.noreply.github.com>
2024-12-26 14:00:02 +0200

    Fix transaction replay issue

--- transaction_verification.go
+++ transaction_verification.go
@@ -1,3 +1,4 @@
 // Add support for Layer-2 solutions
 // Add governance proposal expiration feature
 // Integrate with decentralized prediction markets
+// Add support for multiple consensus mechanisms
